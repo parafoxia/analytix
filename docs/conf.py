5eb2bcff7afb90ffe13b9723f2193df6031281d6
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-01-30 23:35:33 +0000

    Bumped version number
    Also configured conf.py so it takes the version number directly from the library

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,20 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../analytix'))
+
+import analytix
+
+sys.path.insert(0, os.path.abspath("../analytix"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'analytix'
-copyright = '2021, Ethan Henderson'
-author = 'Ethan Henderson'
+project = "analytix"
+copyright = "2021, Ethan Henderson"
+author = "Ethan Henderson"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0a2'
+release = analytix.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -38,12 +41,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,15 +54,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
+    "css_files": [
+        "_static/theme_overrides.css",  # overrides for wide tables in RTD theme
     ],
 }
