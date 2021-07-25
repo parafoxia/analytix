88674e5fda65f30cb2d804718c263a71574c81e0
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-07-25 03:35:37 +0100

    Bump version number and modify description

--- analytix/__init__.py
+++ analytix/__init__.py
@@ -1,6 +1,6 @@
 __productname__ = "analytix"
-__version__ = "2.0.0.dev7"
-__description__ = "A simple yet powerful API wrapper to make getting analytical information from the YouTube Analytics API easier than ever."
+__version__ = "2.0.0"
+__description__ = "A simple yet powerful API wrapper to make getting analytical information from your socials easier than ever."
 __url__ = "https://github.com/parafoxia/analytix"
 __docs__ = "https://analytix.readthedocs.io/en/latest/"
 __author__ = "Ethan Henderson"
--- analytix/packages.py
+++ analytix/packages.py
@@ -2,7 +2,5 @@ __all__ = ("PANDAS_AVAILABLE",)
 
 from pkg_resources import working_set
 
-from analytix.errors import MissingOptionalComponents
-
 _packages = [p.key for p in working_set]
 PANDAS_AVAILABLE = "pandas" in _packages
