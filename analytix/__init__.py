cc30f0b62c9471be57175858826991e45b739838
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-09-26 18:56:12 +0100

    Add on feedback from checks

--- analytix/__init__.py
+++ analytix/__init__.py
@@ -1,11 +1,40 @@
+# Copyright (c) 2021, Ethan Henderson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 __productname__ = "analytix"
-__version__ = "2.1.1"
-__description__ = "A simple yet powerful API wrapper to make getting analytical information from your socials easier than ever."
+__version__ = "2.2.0"
+__description__ = "A simple yet powerful API wrapper to make getting analytical information from the YouTube Analytics API easier than ever."
 __url__ = "https://github.com/parafoxia/analytix"
 __docs__ = "https://analytix.readthedocs.io/en/latest/"
 __author__ = "Ethan Henderson"
-__email__ = "ethan.henderson.1998@gmail.com"
+__author_email__  = "ethan.henderson.1998@gmail.com"
 __license__ = "BSD-3-Clause"
 __bugtracker__ = "https://github.com/parafoxia/analytix/issues"
+__ci__ = "https://github.com/parafoxia/analytix/actions"
 
 from .youtube.analytics.api import YouTubeAnalytics
--- analytix/youtube/analytics/api.py
+++ analytix/youtube/analytics/api.py
@@ -1,21 +1,55 @@
+# Copyright (c) 2021, Ethan Henderson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import csv
 import datetime as dt
 import json
 import logging
 import os
 import time
-import typing as t
-from collections import defaultdict
 
 import requests
 from requests_oauthlib import OAuth2Session
 
-from analytix.errors import *
+from analytix.errors import (
+    HTTPError,
+    InvalidRequest,
+    InvalidScopes,
+    MissingOptionalComponents,
+)
 from analytix.iso import CURRENCIES
-from analytix.packages import *
+from analytix.packages import PANDAS_AVAILABLE
 from analytix.secrets import TOKEN_STORE, YT_ANALYTICS_TOKEN
-from analytix.youtube.analytics import *
-from analytix.youtube.analytics import verify
+from analytix.youtube.analytics import (
+    YOUTUBE_ANALYTICS_API_VERSION,
+    YOUTUBE_ANALYTICS_SCOPES,
+    verify,
+)
 
 if PANDAS_AVAILABLE:
     import pandas as pd
