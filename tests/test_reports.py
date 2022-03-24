c52f3d96e99117796a3dd52185ac103a6cce3794
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-03-24 10:12:19 +0000

    Update tests

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -238,7 +238,9 @@ def test_to_dataframe_modin_check(report):
         mock_cu.return_value = True
 
         with pytest.raises(ImportError):
-            df = report.to_dataframe()
+            df = report.to_dataframe(modin_engine="ray")
+
+        assert os.environ["MODIN_ENGINE"] == "ray"
 
 
 @pytest.mark.skipif(
