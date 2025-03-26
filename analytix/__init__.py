a10695a860d6fb27851bfda34dc9c0bb7ce2d2d4
Ethan Henderson <ethan.henderson.1998@gmail.com>
2025-03-26 11:14:04 +0000

    Bump version number

--- analytix/__init__.py
+++ analytix/__init__.py
@@ -38,7 +38,7 @@ __all__ = (
 )
 
 __productname__ = "analytix"
-__version__ = "5.4.0"
+__version__ = "5.5.0"
 __description__ = "A simple yet powerful SDK for the YouTube Analytics API."
 __url__ = "https://github.com/parafoxia/analytix"
 __docs__ = "https://parafoxia.github.io/analytix"
